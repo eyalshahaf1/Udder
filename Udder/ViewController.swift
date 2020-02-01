3d88c21b60b3be381a68b4cbb41311f7323949f0
Eyal Shahaf <eyal.shahaf1@gmail.com>
2020-02-01 13:39:39 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Eyal Shladov on 01/02/2020.
+//  Copyright © 2020 Eyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
